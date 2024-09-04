7182a326e5281c2efe38f2f2b5578a98f5de6a5c
chenjunpc2008 <chenjunpc2008@hotmail.com>
2024-09-04 09:30:58 +0800

    add client connect timeout func

--- tcpclient/client.go
+++ tcpclient/client.go
@@ -1,146 +1,167 @@
 package tcpclient
 
 import (
-    "fmt"
-    "io"
-    "net"
+	"fmt"
+	"io"
+	"net"
+	"time"
 )
 
 // connect to remote server
 func connectToServer(ip string, port uint16, cli *CtcpCli) error {
-    // const ftag = "connectToServer()"
+	// const ftag = "connectToServer()"
 
-    svrAddr := fmt.Sprintf("%s:%d", ip, port)
-    conn, err := net.Dial("tcp", svrAddr)
-    if nil != err {
-        return err
-    }
+	svrAddr := fmt.Sprintf("%s:%d", ip, port)
+	conn, err := net.Dial("tcp", svrAddr)
+	if nil != err {
+		return err
+	}
 
-    cliNewConnection(conn, ip, port, cli)
+	cliNewConnection(conn, ip, port, cli)
 
-    go cliLoopRead(conn, ip, port, cli, cli.cnf.AsyncReceive)
-    go cliLoopSend(conn, ip, port, cli.chMsgsToBeSend, cli,
-        cli.cnf.RequireSendedCb, cli.cnf.AsyncSended)
+	go cliLoopRead(conn, ip, port, cli, cli.cnf.AsyncReceive)
+	go cliLoopSend(conn, ip, port, cli.chMsgsToBeSend, cli,
+		cli.cnf.RequireSendedCb, cli.cnf.AsyncSended)
 
-    return nil
+	return nil
+}
+
+// connect to remote server
+func connectToServer_Timeout(ip string, port uint16, timeout time.Duration,
+	cli *CtcpCli) error {
+	// const ftag = "connectToServer()"
+
+	svrAddr := fmt.Sprintf("%s:%d", ip, port)
+	conn, err := net.DialTimeout("tcp", svrAddr, timeout)
+	if nil != err {
+		return err
+	}
+
+	cliNewConnection(conn, ip, port, cli)
+
+	go cliLoopRead(conn, ip, port, cli, cli.cnf.AsyncReceive)
+	go cliLoopSend(conn, ip, port, cli.chMsgsToBeSend, cli,
+		cli.cnf.RequireSendedCb, cli.cnf.AsyncSended)
+
+	return nil
 }
 
 func closeCliConn(conn net.Conn, ip string, port uint16, cli *CtcpCli) {
-    conn.Close()
+	conn.Close()
 
-    // notify upper handler
-    cliDisconnected(ip, port, cli)
+	// notify upper handler
+	cliDisconnected(ip, port, cli)
 }
 
 // loop read for one client
 func cliLoopRead(conn net.Conn, ip string, port uint16,
-    cli *CtcpCli, asyncReceive bool) {
-    const ftag = "cliLoopRead()"
+	cli *CtcpCli, asyncReceive bool) {
+	const ftag = "cliLoopRead()"
 
-    defer closeCliConn(conn, ip, port, cli)
+	defer closeCliConn(conn, ip, port, cli)
 
-    var (
-        allbuf            = make([]byte, 0)
-        buffer            = make([]byte, 4096)
-        byAfterDepackBuff []byte
-        lenRcv            int
-        err               error
-    )
+	var (
+		allbuf            = make([]byte, 0)
+		buffer            = make([]byte, 4096)
+		byAfterDepackBuff []byte
+		lenRcv            int
+		err               error
+	)
 
-    for {
-        lenRcv, err = conn.Read(buffer)
+	for {
+		lenRcv, err = conn.Read(buffer)
 
-        if nil != err {
-            select {
-            case <-cli.chExit:
-                // client close
-                return
+		if nil != err {
+			select {
+			case <-cli.chExit:
+				// client close
+				return
 
-            default:
-            }
+			default:
+			}
 
-            if io.EOF == err {
-                errMsgTmp := fmt.Sprintf("%v read from connect EOF, server IP:%s, port:%d", ftag, ip, port)
-                cli.handler.OnErrorStr(errMsgTmp)
-                break
-            } else {
-                errMsgTmp := fmt.Sprintf("%v read from connect failed, server IP:%s, port:%d", ftag, ip, port)
-                cli.handler.OnError(errMsgTmp, err)
-                break
-            }
-        }
+			if io.EOF == err {
+				errMsgTmp := fmt.Sprintf("%v read from connect EOF, server IP:%s, port:%d", ftag, ip, port)
+				cli.handler.OnErrorStr(errMsgTmp)
+				break
+			} else {
+				errMsgTmp := fmt.Sprintf("%v read from connect failed, server IP:%s, port:%d", ftag, ip, port)
+				cli.handler.OnError(errMsgTmp, err)
+				break
+			}
+		}
 
-        if 0 == lenRcv {
-            continue
-        }
+		if 0 == lenRcv {
+			continue
+		}
 
-        allbuf = append(allbuf, buffer[:lenRcv]...)
-        byAfterDepackBuff = cliDataRcved(ip, port, lenRcv, allbuf, cli, asyncReceive)
-        allbuf = byAfterDepackBuff
-    }
+		allbuf = append(allbuf, buffer[:lenRcv]...)
+		byAfterDepackBuff = cliDataRcved(ip, port, lenRcv, allbuf, cli, asyncReceive)
+		allbuf = byAfterDepackBuff
+	}
 
-    // fmt.Printf("%v end loop of client read, server IP:%v, port:%v\n", ftag, ip, port)
+	// fmt.Printf("%v end loop of client read, server IP:%v, port:%v\n", ftag, ip, port)
 }
 
 // loop send for client send
 func cliLoopSend(conn net.Conn, ip string, port uint16,
-    chMsgsToBeSend <-chan interface{}, cli *CtcpCli,
-    requireSendedCb bool, asyncSended bool) {
-    const ftag = "cliLoopSend()"
+	chMsgsToBeSend <-chan interface{}, cli *CtcpCli,
+	requireSendedCb bool, asyncSended bool) {
+	// const ftag = "cliLoopSend()"
 
-    var (
-        bysTobeSend []byte
-        dumyBys     = make([]byte, 0)
-        msg         interface{}
-        ok          bool
-        length      int
-        err         error
-    )
+	var (
+		bysTobeSend []byte
+		dumyBys     = make([]byte, 0)
+		msg         interface{}
+		ok          bool
+		length      int
+		err         error
+	)
 
-    for {
-        bysTobeSend = dumyBys
+	for {
+		bysTobeSend = dumyBys
 
-        select {
-        case <-cli.chExit:
-            // client close
-            //fmt.Printf("%v closing of client chExit-1, ip:%s, port:%d\n", ftag, ip, port)
-            cli.handler.OnEvent("cliLoopSend() exit because exit signal")
-            return
+		select {
+		case <-cli.chExit:
+			// client close
+			//fmt.Printf("%v closing of client chExit-1, ip:%s, port:%d\n", ftag, ip, port)
+			cli.handler.OnEvent("cliLoopSend() exit because exit signal")
+			return
 
-        case msg, ok = <-chMsgsToBeSend:
-            if !ok {
-                cli.handler.OnEvent("cliLoopSend() exit because send channel closed")
-                return
-            }
-        }
+		case msg, ok = <-chMsgsToBeSend:
+			if !ok {
+				cli.handler.OnEvent("cliLoopSend() exit because send channel closed")
+				return
+			}
+		}
 
-        bysTobeSend, err = cli.handler.Pack(msg)
-        if nil != err {
-            cli.handler.OnError("pack failed", err)
-            continue
-        } else if nil == bysTobeSend {
-            cli.handler.OnErrorStr("empty []byte to send")
-            continue
-        }
+		bysTobeSend, err = cli.handler.Pack(msg)
+		if nil != err {
+			cli.handler.OnError("pack failed", err)
+			continue
+		} else if nil == bysTobeSend {
+			cli.handler.OnErrorStr("empty []byte to send")
+			continue
+		}
 
-        length, err = conn.Write(bysTobeSend)
-        if nil != err {
-            select {
-            case <-cli.chExit:
-                // client close
-                //fmt.Printf("%v closing of client chExit-3, ip:%s, port:%d\n", ftag, ip, port)
-                return
+		length, err = conn.Write(bysTobeSend)
+		if nil != err {
+			select {
+			case <-cli.chExit:
+				// client close
+				//fmt.Printf("%v closing of client chExit-3, ip:%s, port:%d\n", ftag, ip, port)
+				return
 
-            default:
-            }
+			default:
+			}
 
-            errMsgTmp := fmt.Sprintf("write failed, msg=%v", msg)
-            cli.handler.OnError(errMsgTmp, err)
-            continue
-        }
+			errMsgTmp := fmt.Sprintf("write failed, msg=%v", msg)
+			cli.handler.OnError(errMsgTmp, err)
+			continue
+		}
 
-        cliDataSended(ip, port, msg, bysTobeSend, length, cli, requireSendedCb, asyncSended)
-    }
+		cliDataSended(ip, port, msg, bysTobeSend, length, cli, requireSendedCb, asyncSended)
+	}
 
-    //fmt.Printf("%v end loop of client send, ip:%s, port:%d\n", ftag, ip, port)
+	//fmt.Printf("%v end loop of client send, ip:%s, port:%d\n", ftag, ip, port)
 }
--- tcpclient/ctcpclient.go
+++ tcpclient/ctcpclient.go
@@ -1,123 +1,129 @@
 package tcpclient
 
 import (
-    "errors"
-    "net"
-    "sync"
+	"errors"
+	"net"
+	"sync"
+	"time"
 )
 
 const (
-    // DefaultSendBuffSize default send buff size
-    DefaultSendBuffSize = 1 * 10000
+	// DefaultSendBuffSize default send buff size
+	DefaultSendBuffSize = 1 * 10000
 )
 
 // Config extra config
 type Config struct {
-    // tcp send buff size
-    SendBuffsize int
+	// tcp send buff size
+	SendBuffsize int
 
-    // after recieve a whole package, the receive callback will go async or sync
-    AsyncReceive bool
+	// after recieve a whole package, the receive callback will go async or sync
+	AsyncReceive bool
 
-    // after data write to tcp sys buff, do OnSendedData() call back require
-    RequireSendedCb bool
-    // sended callback if async or sync
-    AsyncSended bool
+	// after data write to tcp sys buff, do OnSendedData() call back require
+	RequireSendedCb bool
+	// sended callback if async or sync
+	AsyncSended bool
 }
 
 // DefaultConfig default Config
 func DefaultConfig() Config {
-    return Config{
-        SendBuffsize:    DefaultSendBuffSize,
-        AsyncReceive:    true,
-        RequireSendedCb: true,
-        AsyncSended:     true,
-    }
+	return Config{
+		SendBuffsize:    DefaultSendBuffSize,
+		AsyncReceive:    true,
+		RequireSendedCb: true,
+		AsyncSended:     true,
+	}
 }
 
 // CtcpCli tcp client
 type CtcpCli struct {
-    svrIP   string
-    svrPort uint16
-    cnf     Config
+	svrIP   string
+	svrPort uint16
+	cnf     Config
 
-    conn         net.Conn
-    bIsConnected bool
-    handler      EventHandler
-    chExit       chan int // notify all goroutines to shutdown
+	conn         net.Conn
+	bIsConnected bool
+	handler      EventHandler
+	chExit       chan int // notify all goroutines to shutdown
 
-    lock           sync.Mutex // lock for below values
-    chMsgsToBeSend chan interface{}
+	lock           sync.Mutex // lock for below values
+	chMsgsToBeSend chan interface{}
 }
 
 // New new tcp client
 func New(eventCb EventHandler, cnf Config) *CtcpCli {
 
-    var cli = &CtcpCli{bIsConnected: false}
+	var cli = &CtcpCli{bIsConnected: false}
 
-    cli.chExit = make(chan int)
-    cli.handler = eventCb
-    cli.cnf = cnf
-    cli.chMsgsToBeSend = make(chan interface{}, cli.cnf.SendBuffsize)
+	cli.chExit = make(chan int)
+	cli.handler = eventCb
+	cli.cnf = cnf
+	cli.chMsgsToBeSend = make(chan interface{}, cli.cnf.SendBuffsize)
 
-    return cli
+	return cli
 }
 
 // ConnectToServer connect to remote server
 func (cli *CtcpCli) ConnectToServer(ip string, port uint16) error {
-    return connectToServer(ip, port, cli)
+	return connectToServer(ip, port, cli)
+}
+
+// ConnectToServer connect to remote server
+func (cli *CtcpCli) ConnectToServer_Timeout(ip string, port uint16, timeout time.Duration) error {
+	return connectToServer_Timeout(ip, port, timeout, cli)
 }
 
 // Close close connection
 func (cli *CtcpCli) Close() {
-    // lock
-    cli.lock.Lock()
-    defer cli.lock.Unlock()
+	// lock
+	cli.lock.Lock()
+	defer cli.lock.Unlock()
 
-    if nil != cli.conn {
-        close(cli.chExit)
-        cli.conn.Close()
-        cli.conn = nil
-    }
+	if nil != cli.conn {
+		close(cli.chExit)
+		cli.conn.Close()
+		cli.conn = nil
+	}
 
-    cli.svrIP = ""
-    cli.svrPort = 0
-    cli.bIsConnected = false
+	cli.svrIP = ""
+	cli.svrPort = 0
+	cli.bIsConnected = false
 
-    if nil != cli.chMsgsToBeSend {
-        close(cli.chMsgsToBeSend)
-        cli.chMsgsToBeSend = nil
-    }
+	if nil != cli.chMsgsToBeSend {
+		close(cli.chMsgsToBeSend)
+		cli.chMsgsToBeSend = nil
+	}
 }
 
 // new conn
 func (cli *CtcpCli) addNewConnection(conn net.Conn, ip string, port uint16) {
-    // lock
-    cli.lock.Lock()
-    defer cli.lock.Unlock()
+	// lock
+	cli.lock.Lock()
+	defer cli.lock.Unlock()
 
-    cli.conn = conn
-    cli.svrIP = ip
-    cli.svrPort = port
-    cli.bIsConnected = true
-    cli.chMsgsToBeSend = make(chan interface{}, cli.cnf.SendBuffsize)
+	cli.conn = conn
+	cli.svrIP = ip
+	cli.svrPort = port
+	cli.bIsConnected = true
+	cli.chMsgsToBeSend = make(chan interface{}, cli.cnf.SendBuffsize)
 }
 
 // disconnected info update
 func (cli *CtcpCli) disconnected() {
-    // lock
-    cli.lock.Lock()
-    defer cli.lock.Unlock()
+	// lock
+	cli.lock.Lock()
+	defer cli.lock.Unlock()
 
-    cli.conn = nil
-    cli.svrIP = ""
-    cli.svrPort = 0
-    cli.bIsConnected = false
+	cli.conn = nil
+	cli.svrIP = ""
+	cli.svrPort = 0
+	cli.bIsConnected = false
 
-    if nil != cli.chMsgsToBeSend {
-        close(cli.chMsgsToBeSend)
-        cli.chMsgsToBeSend = nil
-    }
+	if nil != cli.chMsgsToBeSend {
+		close(cli.chMsgsToBeSend)
+		cli.chMsgsToBeSend = nil
+	}
 }
 
 /*
@@ -126,34 +132,34 @@ SendToServer send message to server
 @return busy bool : true -- buff is full, you may need to try again
 */
 func (cli *CtcpCli) SendToServer(msg interface{}) (busy bool, retErr error) {
-    // lock
-    cli.lock.Lock()
+	// lock
+	cli.lock.Lock()
 
-    if !cli.bIsConnected {
-        // unlock
-        cli.lock.Unlock()
-        return false, errors.New("not connected")
-    }
+	if !cli.bIsConnected {
+		// unlock
+		cli.lock.Unlock()
+		return false, errors.New("not connected")
+	}
 
-    if nil == cli.chMsgsToBeSend {
-        // unlock
-        cli.lock.Unlock()
-        return false, errors.New("nil chMsgsToBeSend")
-    }
+	if nil == cli.chMsgsToBeSend {
+		// unlock
+		cli.lock.Unlock()
+		return false, errors.New("nil chMsgsToBeSend")
+	}
 
-    curBuffSize := len(cli.chMsgsToBeSend)
+	curBuffSize := len(cli.chMsgsToBeSend)
 
-    if curBuffSize >= cli.cnf.SendBuffsize-1 {
-        // unlock
-        cli.lock.Unlock()
-        return true, nil
-    }
+	if curBuffSize >= cli.cnf.SendBuffsize-1 {
+		// unlock
+		cli.lock.Unlock()
+		return true, nil
+	}
 
-    // push
-    cli.chMsgsToBeSend <- msg
+	// push
+	cli.chMsgsToBeSend <- msg
 
-    // unlock
-    cli.lock.Unlock()
+	// unlock
+	cli.lock.Unlock()
 
-    return false, nil
+	return false, nil
 }
